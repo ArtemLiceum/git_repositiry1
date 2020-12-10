2da1e3dc6cf36c15ffbdafde1f3255f43155109c
Artem <bedintema@gmail.com>
2020-12-10 17:18:05 +0300

    2

--- program.py
+++ program.py
@@ -1,8 +1,14 @@
+def my_superfunction():
+    print('What an awesome print!')
+    print('hello, friend!')
+
+
 def main():
     print('My first git program')
     print('And I change it every day')
-    print('commit')
-    print()
+    print('Again')
+    print('UFO came and added this line')
+    my_superfunction()
 
 
 if __name__ == '__main__':
